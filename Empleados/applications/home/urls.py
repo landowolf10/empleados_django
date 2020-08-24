181e4110c1e3820574877a6aadb50f6c4a73863f
landowolf10 <orlando.avilag@hotmail.com>
2020-08-24 00:24:42 -0500

    Se sigue con el curso.

--- Empleados/applications/home/urls.py
+++ Empleados/applications/home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('home/', views.PruebaView.as_view()),
     path('lista/', views.PruebaListView.as_view()),
+    path('lista-prueba/', views.ListarPrueba.as_view()),
+    path('add/', views.PruebaCreateView.as_view()),
 ]
--- Empleados/applications/home/views.py
+++ Empleados/applications/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    CreateView
+)
+from .models import Prueba
 
 class PruebaView(TemplateView):
     template_name = 'home/prueba.html'
@@ -9,3 +14,13 @@ class PruebaListView(ListView):
     template_name = "home/lista.html"
     context_object_name = 'listaNumeros'
     queryset = ['0', '10', '20', '30']
+
+class ListarPrueba(ListView):
+    template_name = "home/lista_prueba.html"
+    model = Prueba
+    context_object_name = 'lista'
+    
+class PruebaCreateView(CreateView):
+    template_name = "home/add.html"
+    model = Prueba
+    fields = ['titulo', 'subtitulo', 'cantidad']
